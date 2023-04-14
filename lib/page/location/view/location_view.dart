237b28f37b4ead09ad9c2eb73ae9dbdeb70b958e
kimhokyung <ghrud92@gmail.com>
2023-04-14 22:51:55 +0900

    (EDFJ-82) 검색 UI 구현

--- lib/page/location/view/location_view.dart
+++ lib/page/location/view/location_view.dart
@@ -20,5 +20,58 @@ class LocationView extends StatelessWidget {
             onPressed: () => Navigator.of(context).pop(),
           ),
         ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 22),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (text) {
+                        // TODO(kimhokyung): 동네 검색
+                      },
+                      style: const TextStyle(
+                        color: Color(0xFF222222),
+                        fontSize: 16,
+                      ),
+                      decoration: const InputDecoration(
+                        hintText: '동이름(읍,면)으로 검색',
+                        hintStyle: TextStyle(
+                          color: Color(0xFF999999),
+                          fontSize: 16,
+                        ),
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Color(0xFF999999),
+                        ),
+                        filled: true,
+                        fillColor: Color(0xFFF5F5F5),
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: () {
+                      // TODO(kimhokyung): 위치 권한 요청 & 현재 위치로 동네 변경
+                    },
+                    icon: Image.asset(
+                      'assets/icons/location.png',
+                      width: 45,
+                      height: 45,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // TODO(kimhokyung): 동네 리스트 UI
+          ],
+        ),
       );
 }
