f16ac412ba5525ebc2def01afa455712c7883f73
kimhokyung <ghrud92@gmail.com>
2023-04-25 21:24:18 +0900

    (EDFJ-88) District와 Block에 전체 옵션 추가

--- lib/page/location/cubit/location_cubit.dart
+++ lib/page/location/cubit/location_cubit.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../repository/city/block.dart';
+import '../../../repository/city/city.dart';
 import '../../../repository/city/city_repository.dart';
+import '../../../repository/city/district.dart';
 import 'location_state.dart';
 
 class LocationCubit extends Cubit<LocationState> {
@@ -11,7 +14,31 @@ class LocationCubit extends Cubit<LocationState> {
   final CityRepository repository;
 
   Future<void> initialize() async {
-    final cities = await repository.listCities();
+    final loaded = await repository.listCities();
+    final cities = loaded.map((city) {
+      // District에 전체 옵션 추가
+      final blocksOfAllDistricts =
+          city.districts.expand((district) => district.blocks).toList();
+      final allDistrict = District(
+        name: '전체',
+        blocks: blocksOfAllDistricts,
+      );
+      final districts = [allDistrict, ...city.districts]
+          // District에서 이름이 빈 경우 제거
+          .where((district) => district.name.isNotEmpty)
+          // Block에 전체 옵션 추가
+          .map((district) {
+        final blocks = [const Block(name: '전체'), ...district.blocks];
+        return District(
+          name: district.name,
+          blocks: blocks,
+        );
+      }).toList();
+      return City(
+        name: city.name,
+        districts: districts,
+      );
+    }).toList();
     emit(state.copyWith(cities: cities));
   }
 
