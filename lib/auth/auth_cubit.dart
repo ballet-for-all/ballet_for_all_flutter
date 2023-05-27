90d0df26519f82c5a8c9e61b0b74b49eae948724
kimhokyung <ghrud92@gmail.com>
2023-05-27 18:26:33 +0900

    (EDFJ-99) AuthCubit 유닛 테스트 추가

--- lib/auth/auth_cubit.dart
+++ lib/auth/auth_cubit.dart
@@ -6,11 +6,11 @@ import 'selected_location.dart';
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(const AuthState());
 
-  void updateLocation(String? city, String? district, String? block) {
+  void updateLocation(String city, String? district, String? block) {
     emit(state.copyWith(
       selectedLocation: SelectedLocation(
-        // NOTE: district를 전체를 골랐다면 '서울특별시 청운동' 처럼 구 이름이 비어있을 수 있다. 필요하다면 kakao map api를 사용해서 구 이름을
-        //  가져올 수 있다.
+        // NOTE: district나 block을 전체를 골랐다면 구 또는 동 이름이 비어있을 수 있다.
+        // 구 이름의 경우 필요하다면 kakao map api를 사용해서 구 이름을 가져올 수 있다.
         city: city,
         district: district,
         block: block,
