2c9cf66a587797e565398378c89bf6629cd2e15e
kimhokyung <ghrud92@gmail.com>
2023-06-04 19:34:21 +0900

    (EDFJ-95) Firestore collection 데이터 타입 삭제

--- lib/firebase/firestore_locations/firestore_locations_collection.dart
+++ lib/firebase/firestore_locations/firestore_locations_collection.dart
@@ -1,27 +1,41 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../firestore_client.dart';
-import 'firestore_location.dart';
 
 class FirestoreLocationsCollection {
   FirestoreLocationsCollection(FirestoreClient client)
-      : _ref = client.firestore
-            .collection(_collectionName)
-            .withConverter<FirestoreLocation>(
-              fromFirestore: FirestoreLocation.fromFirestore,
-              toFirestore: (firestoreLocation, options) =>
-                  firestoreLocation.toFirestore(),
-            );
+      : _ref = client.firestore.collection(_collectionName);
 
   static const String _collectionName = 'location';
-  final CollectionReference<FirestoreLocation> _ref;
+  final CollectionReference<Map<String, dynamic>> _ref;
 
+  /// @return
+  /// {
+  ///  cities: [
+  ///   {
+  ///    name: '서울',
+  ///    districts: [
+  ///     {
+  ///      name: '강남구',
+  ///      blocks: [
+  ///       {
+  ///        name: '역삼동'
+  ///       },
+  ///       ...
+  ///      ]
+  ///     },
+  ///     ...
+  ///    ]
+  ///   },
+  ///   ...
+  ///  ]
+  /// }
   Future<Map<String, dynamic>> getLocation() async {
     final snapshot = await _ref.get();
     final locations = snapshot.docs.map((doc) => doc.data()).toList();
     if (locations.length != 1) {
       throw Exception('Location should be only one.');
     }
-    return locations.first.toJson();
+    return locations.first;
   }
 }
