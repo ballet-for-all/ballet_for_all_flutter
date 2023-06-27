6d7feb52124e329dbf39c328764864d11d049de6
kimhokyung <ghrud92@gmail.com>
2023-06-27 21:18:24 +0900

    (EDFJ-102) AcademyListTile 디자인 적용

--- lib/page/main/view/academy_list_tile.dart
+++ lib/page/main/view/academy_list_tile.dart
@@ -62,136 +62,121 @@ class _AcademyListTileState extends State<AcademyListTile> {
         onTap: () {
           // TODO(ghrud92): 학원 상세 페이지로 이동
         },
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                CarouselSlider.builder(
-                  carouselController: _carouselController,
-                  options: CarouselOptions(
-                    height: 219,
-                    viewportFraction: 1,
-                    enableInfiniteScroll: false,
-                    onPageChanged: (index, reason) => setState(() {
-                      _imageIndex = index;
-                    }),
-                  ),
-                  itemCount: widget.academyImages.length,
-                  itemBuilder: (context, index, realIndex) => SizedBox(
-                    width: double.infinity,
-                    child: Image.network(
-                      widget.academyImages[index],
-                      fit: BoxFit.fitWidth,
-                    ),
-                  ),
-                ),
-                Positioned.fill(
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: IconButton(
-                      icon: const Icon(
-                        Icons.chevron_left,
-                        color: Colors.white,
-                      ),
-                      onPressed: () => _carouselController.previousPage(),
-                    ),
-                  ),
-                ),
-                Positioned.fill(
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: IconButton(
-                      icon: const Icon(
-                        Icons.chevron_right,
-                        color: Colors.white,
-                      ),
-                      onPressed: () => _carouselController.nextPage(),
-                    ),
-                  ),
-                ),
-                Positioned.fill(
-                  bottom: 20,
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: DotsIndicator(
-                      dotsCount: widget.academyImages.length,
-                      position: _imageIndex,
-                      decorator: DotsDecorator(
-                        color: Colors.white.withOpacity(0.7),
-                        activeColor: const Color(0xFF222222),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Container(
-              color: Colors.white,
-              padding: const EdgeInsets.all(20),
-              child: Column(
+        child: Container(
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            children: [
+              Stack(
                 children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              widget.academyName,
-                              style: const TextStyle(
-                                color: Color(0xFF333333),
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              widget.academyAddress,
-                              style: const TextStyle(
-                                fontSize: 13,
-                                color: Color(0xFF999999),
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ],
-                        ),
+                  CarouselSlider.builder(
+                    carouselController: _carouselController,
+                    options: CarouselOptions(
+                      height: 202,
+                      viewportFraction: 1,
+                      enableInfiniteScroll: false,
+                      onPageChanged: (index, reason) => setState(() {
+                        _imageIndex = index;
+                      }),
+                    ),
+                    itemCount: widget.academyImages.length,
+                    itemBuilder: (context, index, realIndex) => SizedBox(
+                      width: double.infinity,
+                      child: Image.network(
+                        widget.academyImages[index],
+                        fit: BoxFit.fitWidth,
                       ),
-                      const Icon(
-                        Icons.favorite_border,
-                        color: Color(0xFFE5E5E5),
-                      ),
-                    ],
+                    ),
                   ),
-                  const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: ClassInfo(
-                          title: '정규수업',
-                          description: regularPriceDescription,
-                          exist: widget.regularPrice != null,
+                  Positioned.fill(
+                    bottom: 20,
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: DotsIndicator(
+                        dotsCount: widget.academyImages.length,
+                        position: _imageIndex,
+                        decorator: DotsDecorator(
+                          color: Colors.white.withOpacity(0.7),
+                          activeColor: const Color(0xFF222222),
                         ),
                       ),
-                      Expanded(
-                        child: ClassInfo(
-                          title: '쿠폰수업',
-                          description: couponPriceDescription,
-                          exist: widget.couponPrice != null,
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                  const SizedBox(height: 12),
-                  ClassInfo(
-                    title: '작품반',
-                    description: widget.pieceClassDescription ?? '작품 수업이 없어요',
-                    exist: widget.pieceClassDescription != null,
-                  )
                 ],
               ),
-            ),
-          ],
+              Container(
+                color: Colors.white,
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.academyName,
+                                style: const TextStyle(
+                                  color: Color(0xFF333333),
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Row(
+                                children: [
+                                  const Icon(
+                                    Icons.location_on,
+                                    color: Color(0xFFBBBBBB),
+                                    size: 18,
+                                  ),
+                                  Text(
+                                    widget.academyAddress,
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      color: Color(0xFFBBBBBB),
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Icon(
+                          Icons.favorite_border,
+                          color: Color(0xFFE5E5E5),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    ClassInfo(
+                      title: '정규수업',
+                      description: regularPriceDescription,
+                      exist: widget.regularPrice != null,
+                    ),
+                    const SizedBox(height: 10),
+                    ClassInfo(
+                      title: '쿠폰수업',
+                      description: couponPriceDescription,
+                      exist: widget.couponPrice != null,
+                    ),
+                    const SizedBox(height: 10),
+                    ClassInfo(
+                      title: '작품반',
+                      description: widget.pieceClassDescription ?? '작품 수업이 없어요',
+                      exist: widget.pieceClassDescription != null,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
