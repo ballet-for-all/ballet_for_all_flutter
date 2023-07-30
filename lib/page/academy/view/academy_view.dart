b8a1a4efe522b873048bbfa66bf30c013776f455
kimhokyung <ghrud92@gmail.com>
2023-07-30 20:24:41 +0900

    (EDFJ-107) 상세 페이지 내 스크롤 표현하는 탭 작성

--- lib/page/academy/view/academy_view.dart
+++ lib/page/academy/view/academy_view.dart
@@ -1,4 +1,3 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
@@ -65,11 +64,94 @@ class AcademyView extends StatelessWidget {
                 // TODO(ghrud92): Carousel indicator 추가
                 AcademyDefaultInfo(name: name, address: address),
                 const Divider(
+                  height: 1,
                   color: Color(0xFFE5E5E5),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          TextButton(
+                            // TODO(ghrud92): 스크롤 로직 추가
+                            onPressed: () {},
+                            child: Text(
+                              '강사소개',
+                              // TODO(ghrud92): 현재 보고 있는 위치 계산하는 로직 추가
+                              style: _tabButtonStyle(true),
+                            ),
+                          ),
+                          _tabIndicator(true),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              '시설 사진',
+                              style: _tabButtonStyle(false),
+                            ),
+                          ),
+                          _tabIndicator(false),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              '시간표',
+                              style: _tabButtonStyle(false),
+                            ),
+                          ),
+                          _tabIndicator(false),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              '수강료',
+                              style: _tabButtonStyle(false),
+                            ),
+                          ),
+                          _tabIndicator(false),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const Divider(
+                  height: 7,
+                  thickness: 7,
+                  color: Color(0xFFF4F4F4),
                 ),
               ],
             ),
           ),
         ),
+      );
+
+  TextStyle _tabButtonStyle(bool selected) => TextStyle(
+        fontSize: 16,
+        fontWeight: selected ? FontWeight.w600 : FontWeight.w100,
+        color: selected ? const Color(0xFF222222) : const Color(0xFF999999),
+      );
+
+  Widget _tabIndicator(bool selected) => Container(
+        height: 4,
+        color: selected ? Colors.black : Colors.transparent,
       );
 }
