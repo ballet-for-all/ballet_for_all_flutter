59aa2f87d8d311028b21d9618980c7555eaa6da6
kimhokyung <ghrud92@gmail.com>
2023-09-02 22:48:24 +0900

    (EDFJ-108) 학원 상세페이지에서 실제 데이터 사용

--- lib/firebase/firestore_collection.dart
+++ lib/firebase/firestore_collection.dart
@@ -1,7 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreCollection {
-  Map<String, dynamic> docToMap(QueryDocumentSnapshot doc) {
+  Map<String, dynamic> queryDocumentSnapshotToMap(QueryDocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return <String, dynamic>{
+      ...data,
+      'id': doc.id,
+    };
+  }
+
+  Map<String, dynamic> documentSnapshotToMap(DocumentSnapshot doc) {
     final data = doc.data() as Map<String, dynamic>;
     return <String, dynamic>{
       ...data,
